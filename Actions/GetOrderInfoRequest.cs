b5a8d8ce20507a883047db269fcf132125a2b34e
Adam Humpherys <adam@adamh.us>
2024-11-19 17:09:54 -0700

    order info updates

--- Actions/GetOrderInfoRequest.cs
+++ Actions/GetOrderInfoRequest.cs
@@ -9,9 +9,15 @@ namespace OpenSRS.NET.Actions
     public sealed class GetOrderInfoRequest : OpenSRSRequest
     {
         public GetOrderInfoRequest() : base("get_order_info", "DOMAIN")
-        {
-        }
+        { }
 
         public long Id { get; set; }
+
+        public override Dictionary<string, object> GetParameters()
+        {
+            return new Dictionary<string, object> {
+                { "order_id", Id }
+            };
+        }
     }
 }
